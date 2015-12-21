f98463114a5d5c0b0de7e09375927f569a236678
Yuval Maman <yuvilio@yahoo.com>
2015-12-21 00:12:45 -0500

    set up svg spritesheet add logo

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,20 @@
+
+// Bring in the generated svg stack sprite sheet
+// Source: https://css-tricks.com/ajaxing-svg-sprite/
+var loadSvgs = function(){
+
+  var ajax = new XMLHttpRequest();
+  ajax.open("GET", "/assets/img/stack/svg/sprite.stack.svg", true);
+  ajax.send();
+  ajax.onload = function(e) {
+    var div = document.createElement("div");
+    div.classList.add('svg-stack');
+    div.innerHTML = ajax.responseText;
+    document.body.insertBefore(div, document.body.childNodes[0]);
+  }
+
+};
+
 (function($) {
   $(function(){
     //initialize foundation
@@ -7,6 +24,10 @@
     $('pre code').each(function(i, block) {
       hljs.highlightBlock(block);
     });
-    
-  })
+
+    //svg stacks
+    loadSvgs();
+
+
+  });
 })(jQuery);
